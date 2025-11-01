a8da2ea54c30dd20f77ff4241c585df3f1bb21b8
Sean Liu <seanliuveritas@gmail.com>
2025-11-01 13:39:59 -0400

    LC 235 added

---
+++ LC0235_Lowest_Common_Ancestor_BST/lowest_common_ancestor_bst.py
@@ -0,0 +1,25 @@
+# Problem: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        lca = root
+        while lca:
+            if p.val > lca.val and q.val > lca.val:
+                # Go right if both p and q are greater than lca
+                lca = lca.right
+            elif p.val < lca.val and q.val < lca.val :
+                # Go left if both p and q are smaller than lca
+                lca = lca.left
+            else: 
+                # if one of p and q is greater than root but the other is smaller than root
+                # OR if one of p and q equals lca
+                return lca
+        return lca
+        
