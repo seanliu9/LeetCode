47fb73e5bbe7dfac364fb151667217b506b135a7
Sean Liu <seanliuveritas@gmail.com>
2025-11-26 14:24:14 -0500

    current LC 329

--- LC0329_Longest_Increasing_Path_in_Matrix/longest_increasing_path_in_matrix.py
+++ LC0329_Longest_Increasing_Path_in_Matrix/longest_increasing_path_in_matrix.py
@@ -5,32 +5,33 @@ class Solution:
     # Run BFS from (start_x, start_y), and find the greatest path length from it.
     def bfs(self, matrix: List[List[int]], start_x: int, start_y: int, m: int, n: int):
         queue = deque()
-        distances = {(start_x, start_y): 0}
+        distances = [[0] * n for _ in range(m)]
+        distances[start_x][start_y] = 0
         queue.append((start_x, start_y))
         max_dist = 0
         while len(queue) > 0:
             curr_x, curr_y = queue.popleft()
-            new_dist = distances[(curr_x, curr_y)] + 1
+            new_dist = distances[curr_x][curr_y] + 1
             # Consider all 4 of curr_vtx's valid neighbors (left, up, right, down)
             # left neighbor
             if curr_y - 1 >= 0 and matrix[curr_x][curr_y - 1] > matrix[curr_x][curr_y]:
                 queue.append((curr_x, curr_y - 1))
-                distances[(curr_x, curr_y - 1)] = new_dist
+                distances[curr_x][curr_y - 1] = new_dist
                 max_dist = max(max_dist, new_dist)
             # top neighbor
             if curr_x - 1 >= 0 and matrix[curr_x - 1][curr_y] > matrix[curr_x][curr_y]:
                 queue.append((curr_x - 1, curr_y))
-                distances[(curr_x - 1, curr_y)] = new_dist
+                distances[curr_x - 1][curr_y] = new_dist
                 max_dist = max(max_dist, new_dist)
             # right neighbor
             if curr_y + 1 < n and matrix[curr_x][curr_y + 1] > matrix[curr_x][curr_y]:
                 queue.append((curr_x, curr_y + 1))
-                distances[(curr_x, curr_y + 1)] = new_dist
+                distances[curr_x][curr_y + 1] = new_dist
                 max_dist = max(max_dist, new_dist)
             # bottom neighbor
             if curr_x + 1 < m and matrix[curr_x + 1][curr_y] > matrix[curr_x][curr_y]:
                 queue.append((curr_x + 1, curr_y))
-                distances[(curr_x + 1, curr_y)] = new_dist
+                distances[curr_x + 1][curr_y] = new_dist
                 max_dist = max(max_dist, new_dist)
 
         return max_dist + 1
