8eca826a561add6b524998d1dd000a44afa71a2d
Sean Liu <seanliuveritas@gmail.com>
2026-01-08 11:16:44 -0500

    D & C solution added

--- LC0053_Max_Subarray/max_subarray.go
+++ LC0053_Max_Subarray/max_subarray.go
@@ -39,3 +39,32 @@ func maxSubArray_DP(nums []int) int {
 
 	return int(math.Max(float64(dp_A[n-1]), float64(dp_B[n-1])))
 }
+
+// D&C solution
+func rMaxSubArray(nums []int, start int, end int) [4]int {
+	// base case
+	if start == end {
+		return [4]int{nums[start], nums[start], nums[start], nums[start]}
+	}
+	mid := (start + end) / 2
+	left_answer := rMaxSubArray(nums, start, mid)
+	left_total, left_start, left_end, left_overall := left_answer[0], left_answer[1], left_answer[2], left_answer[3]
+	right_answer := rMaxSubArray(nums, mid+1, end)
+	right_total, right_start, right_end, right_overall := right_answer[0], right_answer[1], right_answer[2], right_answer[3]
+
+	// Merge left and right halves' solutions
+	total := left_total + right_total
+	start_max := int(math.Max(float64(left_start), float64(left_total+right_start)))
+	end_max := int(math.Max(float64(right_end), float64(right_total+left_end)))
+	overall_max := int(math.Max(math.Max(float64(left_overall), float64(right_overall)), float64(left_end+right_start)))
+	return [4]int{total, start_max, end_max, overall_max}
+}
+
+func maxSubArray_DC(nums []int) int {
+	n := len(nums)
+	if n == 1 {
+		return nums[0]
+	}
+	return rMaxSubArray(nums, 0, n-1)[3]
+
+}
--- LC0053_Max_Subarray/max_subarray.py
+++ LC0053_Max_Subarray/max_subarray.py
@@ -32,3 +32,29 @@ class Solution:
             dp_B[i] = max(dp_A[i - 1], dp_B[i - 1])
         
         return max(dp_A[n - 1], dp_B[n - 1])
+    
+    # D&C solution
+    # Finds the max subarray sum in the subarray of nums from start to end (both inclusive)
+    def rMaxSubArray(self, nums: List[int], start: int, end: int):
+        # base case
+        if start == end:
+            return (nums[start], nums[start], nums[start], nums[start])
+            
+        mid = (start + end) // 2
+        left_total, left_start, left_end, left_overall = self.rMaxSubArray(nums, start, mid)
+        right_total, right_start, right_end, right_overall = self.rMaxSubArray(nums, mid + 1, end)
+
+        # When merging, we must consider the case of the answer crossing the boundary.
+        total = left_total + right_total
+        start_max = max(left_start, left_total + right_start)
+        end_max = max(right_end, right_total + left_end)
+        overall_max = max(left_overall, right_overall, left_end + right_start)
+
+        return total, start_max, end_max, overall_max
+
+    def maxSubArray_DC(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        
+        return self.rMaxSubArray(nums, 0, n - 1)[3]
