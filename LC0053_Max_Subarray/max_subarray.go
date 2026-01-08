2b44a28cf07e4f647edc76070d056f5f6ced5f97
Sean Liu <seanliuveritas@gmail.com>
2026-01-08 08:40:27 -0500

    added DP solution in Python and Go

--- LC0053_Max_Subarray/max_subarray.go
+++ LC0053_Max_Subarray/max_subarray.go
@@ -2,6 +2,7 @@ package main
 
 import "math"
 
+// basic solution
 func maxSubArray(nums []int) int {
 	max_sum := int(math.Inf(-1))
 	curr_sum := 0
@@ -16,4 +17,25 @@ func maxSubArray(nums []int) int {
 	}
 
 	return max_sum
+}
+
+// DP solution
+func maxSubArray_DP(nums []int) int {
+	n := len(nums)
+	if n == 1 {
+		return nums[0]
+	}
+
+	dp_A := make([]int, n) // dp_A[i] = max subarray sum when we MUST include nums[i]
+	dp_B := make([]int, n) // dp_B[i] = max subarray sum when we CANNOT include nums[i]
+
+	dp_A[0] = nums[0]
+	dp_B[0] = nums[0]
+
+	for i := 1; i < n; i++ {
+		dp_A[i] = int(math.Max(float64(dp_A[i-1]+nums[i]), float64(nums[i])))
+		dp_B[i] = int(math.Max(float64(dp_A[i-1]), float64(dp_B[i-1])))
+	}
+
+	return int(math.Max(float64(dp_A[n-1]), float64(dp_B[n-1])))
 }
--- LC0053_Max_Subarray/max_subarray.py
+++ LC0053_Max_Subarray/max_subarray.py
@@ -1,6 +1,7 @@
 from typing import List
 
 class Solution:
+    # basic solution
     def maxSubArray(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
@@ -13,3 +14,21 @@ class Solution:
             curr_sum = max(0, curr_sum)
         
         return max_sum
+    
+    # DP solution
+    def maxSubArray_DP(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        
+        dp_A = [0] * n # dp_A[i] = max subarray sum when we MUST include nums[i]
+        dp_B = [0] * n # dp_B[i] = max subarray sum when we CANNOT include nums[i]
+
+        dp_A[0] = nums[0]
+        dp_B[0] = nums[0]
+
+        for i in range(1, n):
+            dp_A[i] = max(dp_A[i - 1] + nums[i], nums[i])
+            dp_B[i] = max(dp_A[i - 1], dp_B[i - 1])
+        
+        return max(dp_A[n - 1], dp_B[n - 1])
