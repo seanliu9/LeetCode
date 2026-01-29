51cce02f693beff13988d1606e13482074ae433f
Sean Liu <seanliuveritas@gmail.com>
2026-01-29 12:50:38 -0500

    LC 2487 passing all C++ tests now

--- LC2487_Remove_Nodes_from_Linked_List/remove_nodes_from_linked_list.cpp
+++ LC2487_Remove_Nodes_from_Linked_List/remove_nodes_from_linked_list.cpp
@@ -10,77 +10,28 @@ struct ListNode {
 using namespace std;
 
 class Solution {
-private:
-    int max_right;
-
 public:
-    Solution()
-    {
-        this->max_right = 0;
-    }
-
-    ListNode* rRemoveNodes(ListNode* curr, ListNode* prev)
+    ListNode* rRemoveNodes(ListNode* curr)
     {
         if (curr->next == nullptr)
         {
             // base case
-            this->max_right = curr->val;
             return curr;
+        }
+        curr->next = this->rRemoveNodes(curr->next);
+        if (curr->val < curr->next->val)
+        {
+            // if we need to remove curr
+            return curr->next;
         }
         else
         {
-            this->rRemoveNodes(curr->next, curr);
-            if (curr->val < this->max_right)
-            {
-                // Remove curr.
-                if (prev == nullptr)
-                {
-                    // if we need to remove curr but it's the first node in the list
-                    ListNode* result = curr->next;
-                    delete curr;
-                    return result;
-                }
-                else
-                {
-                    prev->next = curr->next;
-                    delete curr;
-                    return prev->next;
-                }
-            }
-            else
-            {
-                // no need to remove curr
-                this->max_right = curr->val;
-                return curr;
-            }
+            // no need to remove curr
+            return curr;
         }
     }
 
     ListNode* removeNodes(ListNode* head) {
-        return this->rRemoveNodes(head, nullptr);
+        return this->rRemoveNodes(head);
     }
 };
-
-int main()
-{
-    ListNode* head = new ListNode(5);
-    head->next = new ListNode(2);
-    head->next->next = new ListNode(13);
-    head->next->next->next = new ListNode(3);
-    head->next->next->next->next = new ListNode(8);
-
-    
-    // ListNode* head = new ListNode(3);
-    // head->next = new ListNode(4);
-    // head->next->next = new ListNode(6);
-    // head->next->next->next = new ListNode(150);
-
-    Solution sol;
-    ListNode* answer = sol.removeNodes(head);
-    ListNode* curr = answer;
-    while (curr != nullptr)
-    {
-        cout << curr->val << "->";
-        curr = curr->next;
-    }
-}
