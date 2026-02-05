ea39f6adb87086038c8a97adb1c0e507ebee08a2
Sean Liu <seanliuveritas@gmail.com>
2026-02-05 13:31:55 -0500

    updated LC 222 C++

--- LC0222_Count_Complete_Tree_Nodes/count_tree_nodes.cpp
+++ LC0222_Count_Complete_Tree_Nodes/count_tree_nodes.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -9,13 +11,43 @@ struct TreeNode {
 
 class Solution {
 public:
-    int rCountNodes(const TreeNode* const node) const
+    int r_left_height(const TreeNode* const node) const
     {
         if (node == nullptr)
         {
             return 0;
         }
-        return 1 + this->rCountNodes(node->left) + this->rCountNodes(node->right);
+        else
+        {
+            return 1 + this->r_left_height(node->left);
+        }
+    }
+
+    int r_right_height(const TreeNode* const node) const
+    {
+        if (node == nullptr)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1 + this->r_right_height(node->right);
+        }
+    }
+
+    int rCountNodes(const TreeNode* const node) const
+    {
+        int l_height = this->r_left_height(node);
+        int r_height = this->r_right_height(node);
+        if (l_height == r_height)
+        {
+            // Use property of perfect binary tree.
+            return pow(2, l_height) - 1;
+        }
+        else
+        {
+            return 1 + this->rCountNodes(node->left) + this->rCountNodes(node->right);
+        }
     }
 
     int countNodes(TreeNode* root) {
