c95f43b071074ec3fa135bca12756fc050b4b1f9
Sean Liu <seanliuveritas@gmail.com>
2026-02-06 11:54:18 -0500

    LC 617 Merge 2 Binary Trees

---
+++ LC0617_Merge_Binary_Trees/merge_binary_trees.cpp
@@ -0,0 +1,30 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* rMergeTrees(const TreeNode* root1, const TreeNode* root2)
+    {
+        if (root1 == nullptr && root2 == nullptr)
+        {
+            return nullptr;
+        }
+        else
+        {
+            TreeNode* result = new TreeNode((root1 == nullptr ? 0 : root1->val) + (root2 == nullptr ? 0 : root2->val));
+            result->left = this->rMergeTrees(root1 == nullptr ? nullptr : root1->left, root2 == nullptr ? nullptr : root2->left);
+            result->right = this->rMergeTrees(root1 == nullptr ? nullptr : root1->right, root2 == nullptr ? nullptr : root2->right);
+            return result;
+        }
+    }
+
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        return this->rMergeTrees(root1, root2);
+    }
+};
